c22868418e3037f0397b927a0e76501fe2733ff4
absy72 <ali@example.com>
2023-02-12 05:15:20 -0800

    sum and minus operation

---
+++ src/main/java/org/example/SumMinus.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class SumMinus {
+
+    public int sum(int num1,int num2){
+
+
+        return num1+num2;
+    }
+
+    public int minus(int num1,int num2){
+
+
+        return num1-num2;
+    }
+
+}
